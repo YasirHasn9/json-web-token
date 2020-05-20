df5ff05c1eab5c62aba7c9abdcf57c050bced763
YasirHasn9 <hassanyasir223@gmail.com>
2020-05-19 21:09:14 -0700

    validate token by restricted function

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,4 +1,24 @@
-module.exports = (req, res, next) => {
+const jwt = require("jsonwebtoken");
+module.exports = () => {
   // add code here to verify users are logged in
-  next();
+  return async (req, res, next) => {
+    const authError = {
+      message: "Invalid credentials"
+    };
+    try {
+      const token = req.headers.authorization;
+      if (!token) {
+        return res.status(401).json(authError);
+      }
+      jwt.verify(token, process.env.JWT_SECRET, (err, decodedPayload) => {
+        if (err) {
+          return res.status(401).json(authError);
+        }
+        req.token = decodedPayload;
+        next();
+      });
+    } catch (err) {
+      next(err);
+    }
+  };
 };
