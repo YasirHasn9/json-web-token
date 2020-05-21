06060b9d4fe7e8ece2ead01469be23119f6fb80e
YasirHasn9 <hassanyasir223@gmail.com>
2020-05-20 23:16:50 -0700

    called a restricted with a role parameter

--- middleware/restricted.js
+++ middleware/restricted.js
@@ -2,40 +2,8 @@ const jwt = require("jsonwebtoken");
 module.exports = {
   restricted
 };
-
-// function restricted() {
-//   return (req, res, next) => {
-//     const authError = {
-//       message: "Invalid Credentials"
-//     };
-//     try {
-//       // to validate the token , first we need to get it somehow
-//       // when the client makes a subsequent request , they would
-//       // send a headers object that we can assign authorization the object on the
-//       // req object
-//       const token = req.headers.authorization;
-//       if (!token) {
-//         return res.status(401).json(authError);
-//       }
-
-//       jwt.verify(token, process.env.TOKEN_SECRET, (err, decodedPayload) => {
-//         if (err) {
-//           return res.status(401).json(authError);
-//         } else {
-//           // in some middle wars we want to get access
-//           // to the some of the info on the user
-//           req.toke = decodedPayload;
-//           next();
-//         }
-//       });
-//     } catch (err) {
-//       next(err);
-//     }
-//   };
-// }
-
 // first we declare  a function
-function restricted() {
+function restricted(role) {
   // this is function return a function
   return async (req, res, next) => {
     try {
@@ -57,7 +25,11 @@ function restricted() {
         res.status(401).json(authError);
       }
       jwt.verify(token, process.env.TOKEN_SECRET, (err, decodedPayload) => {
+        req.token = decodedPayload;
         if (err) {
+          return res.status(401).json(authError);
+        }
+        if (!(decodedPayload.userRole = role && role === "admin")) {
           return res.status(401).json(authError);
         }
         next();
--- users/users-router.js
+++ users/users-router.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const Users = require("./users-models");
 const { restricted } = require("../middleware/restricted");
 
-router.get("/", restricted(), async (req, res, next) => {
+router.get("/", restricted("admin"), async (req, res, next) => {
   try {
     const users = await Users.find();
     res.json(users);
